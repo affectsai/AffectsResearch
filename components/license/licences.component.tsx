679702a01843a8a1d4cbf99131bb991624323ffb
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2024-02-23 18:28:54 -0500

    License name was wrong in the header comments (just in the wrong order)

--- components/license/licences.component.tsx
+++ components/license/licences.component.tsx
@@ -2,9 +2,9 @@
  * Copyright (C) 2024 Affects AI LLC - All Rights Reserved
  *
  * You may use, distribute and modify this code under the terms of
- * the CC BY-SA-NC 4.0 license.
+ * the CC BY-NC-SA 4.0 license.
  *
- * You should have received a copy of the CC BY-SA-NC 4.0 license
+ * You should have received a copy of the CC BY-NC-SA 4.0 license
  * with this file. If not, please write to info@affects.ai or
  * visit:
  *    https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
