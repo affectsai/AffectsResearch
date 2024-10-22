67292b624d198fe50cc052ffa32c9063f196b6ad
Tim Sweeney-Fanelli <tim@zerobytellc.com>
2024-10-22 12:33:11 -0400

    Fixing private key permissions for scp of cuads media file

--- eas-build-hooks/post-install.sh
+++ eas-build-hooks/post-install.sh
@@ -7,6 +7,7 @@ if [ -z "${MEDIA_RETRIEVAL_PRIVATE_KEY}" ]; then
   echo "WARNING: MEDIA_RETRIEVAL_PRIVATE_KEY not set ... retrieval of CUADS media bundle may fail."
   scp -o StrictHostKeyChecking=no tim_affects_ai@api.affects.ai:~/cuads_media.tgz ${EAS_BUILD_WORKINGDIR}/assets/media/cuads_media.tgz
 else
+  chmod 400 ${MEDIA_RETRIEVAL_PRIVATE_KEY}
   scp -o StrictHostKeyChecking=no -i ${MEDIA_RETRIEVAL_PRIVATE_KEY} tim_affects_ai@api.affects.ai:~/cuads_media.tgz ${EAS_BUILD_WORKINGDIR}/assets/media/cuads_media.tgz
 fi
 
